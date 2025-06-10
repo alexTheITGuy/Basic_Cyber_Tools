4b1618e377d8727f5b280616ec0e35a1c95a66ac
Alex Ochoa <aaochoa6@asu.edu>
2025-06-10 15:08:33 -0600

    Update Network_Scanner.py

--- Network_Scanner.py
+++ Network_Scanner.py
@@ -1,9 +1,13 @@
 from scapy.all import ARP, Ether, srp
 
-print('Please enter a valid IP address with subnet. Example: 192.168.0.1/24')
+print('Please enter a valid IP address with subnet in CIDR notation. Example: 192.168.0.0/24')
 
 #Destination IP 
+
 target_ip = input()
+
+if('.' not in target_ip or '/' not in target_ip):
+    raise Exception('Please enter a valid IP address')
 
 #Create arp packet 
 arp = ARP(pdst=target_ip)
