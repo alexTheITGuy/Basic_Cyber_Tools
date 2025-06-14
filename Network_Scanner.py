c7950070bfc590ad729b774f72fc784f86dea74a
Alex Ochoa <aaochoa6@asu.edu>
2025-06-13 23:57:16 -0600

    Update Network_Scanner.py

--- Network_Scanner.py
+++ Network_Scanner.py
@@ -1,39 +1,48 @@
 from scapy.all import ARP, Ether, srp
+import sys
 
-#Destination IP 
-target_ip = input('Please enter a valid IP address with subnet in CIDR notation. Example: 192.168.0.0/24 =============> ')
+def Network_Scan():
+    #Destination IP 
+    target_ip = input('Please enter a valid IP address with subnet in CIDR notation. Example: 192.168.0.0/24 ==> ')
 
-#Check to verify that IP address is valid
-if target_ip.count('.') != 3:
-    raise Exception('Please enter a valid IP address.')
+    #Check to verify that IP address is valid
+    if target_ip.count('.') != 3 or target_ip.count('/') != 1:
+        print('IP not valid')
+        Network_Scan()
+    
+    #Create arp packet 
+    arp = ARP(pdst=target_ip)
+    #Create broadcast MAC address packet
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 
-if target_ip.count('/') != 1:
-    raise Exception('Please enter a valid IP address.')
+    #stack them 
+    packet = ether/arp
 
-#Create arp packet 
-arp = ARP(pdst=target_ip)
+    #srp works at the data link lay (layer 2)
+    result = srp(packet, timeout = 3)[0]
 
-#Create broadcast MAC address packet
-ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    # a list of clients, we will fill this in the upcoming loop
+    clients = []
 
-#stack them 
-packet = ether/arp
+    for sent, received in result: 
+        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
 
-#srp works at the data link lay (layer 2)
-result = srp(packet, timeout = 3)[0]
+    #Print Clients
+    print('Here are the devices in this network')
+    print('IP' + " " * 18 + 'MAC')
+    for client in clients:
+        print("{:16}    {}".format(client['ip'], client['mac']))
 
-# a list of clients, we will fill this in the upcoming loop
-clients = []
-
-for sent, received in result: 
-    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
-
-#Print Clients
-print('Here are the devices in this network')
-print('IP' + " " * 18 + 'MAC')
-for client in clients:
-    print("{:16}    {}".format(client['ip'], client['mac']))
+#TODO
+def Network_Ping():
+    print('Network Ping')
 
 
-
-
+action = int(input('Select an operation ==> '))
+match action:
+        case 0: 
+            sys.exit()
+        case 1: 
+            Network_Scan()
+        case 2:
+            Network_Ping()
