d7afa3ce45da689ebe42612742b75a4476260993
Alex Ochoa <aaochoa6@asu.edu>
2025-06-14 21:01:11 -0600

    Update Network_Scanner.py

--- Network_Scanner.py
+++ Network_Scanner.py
@@ -1,9 +1,18 @@
 from scapy.all import ARP, Ether, srp
 import sys
+import subprocess
+import os
+import ipaddress
+
+print('Exit:                                                    0')
+print('Network scanner (match MAC to IPv4 address):             1')
+print('Get a list of all possible IPs based on network address: 2')
+print('Ping an IPv4 address on the network:                     3')
+print('===========================================================')
 
 def Network_Scan():
     #Destination IP 
-    target_ip = input('Please enter a valid IP address with subnet in CIDR notation. Example: 192.168.0.0/24 ==> ')
+    target_ip = input('Enter a valid IP address with subnet in CIDR notation. Example: 192.168.16.0/24 ==> ')
 
     #Check to verify that IP address is valid
     if target_ip.count('.') != 3 or target_ip.count('/') != 1:
@@ -33,16 +42,37 @@ def Network_Scan():
     for client in clients:
         print("{:16}    {}".format(client['ip'], client['mac']))
 
-#TODO
-def Network_Ping():
-    print('Network Ping')
+#This function gets a list of all IP's on a network based on the network address
+def Get_IP_List():
+    network_address = input("Enter your current IPv4 network address in CIDR notation (Example: 192.168.16.0/24) ==> ")
+    all_ipv4 = ipaddress.ip_network(network_address)
 
+    for host in all_ipv4.hosts():
+        if not host.is_private:
+            continue
+        print(host)
+
+#Used to ping IP address on the conected network
+def Network_Ping():
+    #Gets the current OS that the script is running on
+    current_os = os.name
+    ip_address = input('What IP address would you like to ping? ==> ')
+
+    #If the current OS is Windows 
+    if current_os == 'nt':
+        output = subprocess.run(['ping', '-n', '4', ip_address])
+    #If the current OS is Linux based
+    else:
+        output = subprocess.run(['ping', '-c', '4', ip_address])
 
 action = int(input('Select an operation ==> '))
+
 match action:
-        case 0: 
-            sys.exit()
-        case 1: 
-            Network_Scan()
-        case 2:
-            Network_Ping()
+    case 0: 
+        sys.exit()
+    case 1: 
+        Network_Scan()
+    case 2:
+        Get_IP_List()
+    case 3:
+        Network_Ping()
